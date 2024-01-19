8373ba8aafca29ffc2063d6cf46000233678a7c3
reitmas32 <rafa.zamora.rals@gmail.com>
2024-01-18 23:56:00 -0600

    fix: remove unused imports

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:unicon/ui/organism/background.dart';
-import 'package:unicon/ui/organism/count_down.dart';
 import 'package:unicon/ui/organism/home_body.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
-import 'package:unicon/ui/organism/speakers.dart';
-import 'package:unicon/ui/organism/talks.dart';
 import 'package:unicon/ui/providers/scroll.dart';
 
 class HomePage extends ConsumerWidget {
