0f445461b5cf60230d66b33070c15ab45ef4f4b2
reitmas32 <rafa.zamora.rals@gmail.com>
2024-01-24 23:13:07 -0600

    feat: update SpeakerModel

--- lib/domain/models/speaker.dart
+++ lib/domain/models/speaker.dart
@@ -1,7 +1,15 @@
 class SpeakerModel {
-  SpeakerModel({required this.img, required this.name, required this.job});
+  SpeakerModel({
+    required this.img,
+    required this.name,
+    required this.tags,
+    required this.description,
+    required this.job,
+  });
 
   final String img;
   final String name;
+  final String description;
+  final List<String> tags;
   final String job;
 }
