08c872d4e1f158f3968103354038c115b6a5d3d5
reitmas32 <rafa.zamora.rals@gmail.com>
2024-01-24 23:15:08 -0600

    feat: create scrollController

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:unicon/ui/atoms/auth_button.dart';
-import 'package:unicon/ui/molecules/navigation_scroll.dart';
+import 'package:section_scroll/section_scroll.dart';
 import 'package:unicon/ui/organism/background.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
-import 'package:unicon/ui/organism/home_body.dart';
-import 'package:unicon/ui/organism/register_dialog.dart';
-import 'package:unicon/ui/organism/siging_dialog.dart';
-import 'package:unicon/ui/providers/scroll.dart';
 import 'package:unicon/ui/providers/sections.dart';
 
 class HomePage extends ConsumerStatefulWidget {
@@ -21,102 +14,38 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final contentScrollController = ref.watch(scrollProvider);
-
     final sections = ref.watch(sectionsProvider);
     final size = MediaQuery.of(context).size;
     return Scaffold(
       body: BackGroundWidget(
         child: Stack(
           children: [
-            ...buttonsLayer(),
             Column(
               children: [
                 SizedBox(
                   height: size.height * 0.13,
                 ),
                 Expanded(
-                  child: ScrollablePositionedList.builder(
-                    itemScrollController: contentScrollController,
-                    shrinkWrap: true,
-                    itemCount: sections.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return sections[index].content;
-                    },
+                  child: SectionScroll(
+                    controller: scrollController,
+                    children: [...sections.map((e) => e.content).toList()],
                   ),
                 ),
               ],
             ),
-            const Align(
-                alignment: Alignment.centerRight, child: NavigatorScroll()),
+            //const AuthLayer(),
           ],
         ),
       ),
     );
   }
-
-  List<Widget> buttonsLayer() {
-    final buttons = [
-      SimpleButtonModel(
-        lable: "Log In",
-        onPressed: () => sigInDialog(),
-      ),
-      SimpleButtonModel(
-        lable: "Register",
-        onPressed: () => registerDialog(),
-      ),
-    ];
-
-    return [
-      Positioned(
-        top: 80,
-        right: 80,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: buttons
-              .map((e) => [
-                    AuthButton(
-                      label: e.lable,
-                      onPress: e.onPressed,
-                    ),
-                    if (e != buttons.last)
-                      Text(
-                        " | ",
-                        style: GoogleFonts.jetBrainsMono(
-                          fontSize: 27,
-                          fontWeight: FontWeight.w100,
-                          color: Colors.white,
-                        ),
-                      ),
-                  ])
-              .expand((element) => element)
-              .toList(),
-        ),
-      ),
-    ];
-  }
-
-  void goToIndex(int index) {
-    final contentScrollController = ref.read(scrollProvider);
-    contentScrollController.scrollTo(
-      index: index,
-      duration: const Duration(milliseconds: 500),
-    );
-  }
-
-  void sigInDialog() => showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return const SigInDialog();
-        },
-      );
-
-  void registerDialog() => showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return const RegisterDialog();
-        },
-      );
 }
