f20727e6a119e0e2624a4ebd61388e45444b573d
reitmas32 <rafa.zamora.rals@gmail.com>
2024-01-24 23:14:33 -0600

    feat: create AuthLayer

---
+++ lib/ui/organism/auth_layer.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:unicon/domain/models/simple_button.dart';
+import 'package:unicon/ui/atoms/auth_button.dart';
+import 'package:unicon/ui/organism/register_dialog.dart';
+import 'package:unicon/ui/organism/siging_dialog.dart';
+
+class AuthLayer extends StatelessWidget {
+  const AuthLayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final buttons = [
+      SimpleButtonModel(
+        lable: "Log In",
+        onPressed: () => sigInDialog(context),
+      ),
+      SimpleButtonModel(
+        lable: "Register",
+        onPressed: () => registerDialog(context),
+      ),
+    ];
+
+    return Positioned(
+      top: 80,
+      right: 80,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: buttons
+            .map((e) => [
+                  AuthButton(
+                    label: e.lable,
+                    onPress: e.onPressed,
+                  ),
+                  if (e != buttons.last)
+                    Text(
+                      " | ",
+                      style: GoogleFonts.jetBrainsMono(
+                        fontSize: 27,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.white,
+                      ),
+                    ),
+                ])
+            .expand((element) => element)
+            .toList(),
+      ),
+    );
+  }
+
+  void sigInDialog(BuildContext context) => showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const SigInDialog();
+        },
+      );
+
+  void registerDialog(BuildContext context) => showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const RegisterDialog();
+        },
+      );
+}
